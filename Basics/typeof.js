ae98392bdf70798ad33b1ec1ceaf80b717118ea4
sourabh1073 <128901042+sourabh1073@users.noreply.github.com>
2024-02-17 17:43:33 +0000

    typeof some datatype basics conversion

---
+++ Basics/typeof.js
@@ -0,0 +1,24 @@
+const num=183
+let myname="Saurabh"
+const bigInt=true
+
+///console.log(typeof myname);
+///console.log(typeof num);
+///console.log(typeof bigInt);
+
+// => typeof to check datatype
+
+//=> Conbersion of datatype
+
+let num1=Number(bigInt)
+//console.log(Number(num));
+//console.log(num1);
+// console.log(2>1);
+ //console.log(1+3+"33");
+ //console.log(1+"44"+6);
+ //console.log("2"+4+6);  //depend on first value datatype
+
+ console.log(true);  //=>true
+ console.log(+true);  //=>1
+ console.log(+"");  //=>0  beacause value getting converted because of (+) into number
+
